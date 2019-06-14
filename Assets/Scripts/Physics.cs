3d451ffe7e093e91c4ad31e752cc99e32187fd08
Josh Shinnick <asuraengine@hotmail.com>
2019-06-14 18:58:41 +0800

    Laser Bloody Working
    It works finally.

--- Assets/Scripts/Physics.cs
+++ Assets/Scripts/Physics.cs
@@ -6,7 +6,6 @@ public class Physics : MonoBehaviour
 {
     public float forceThrust, maxVelocity = 8, rotationSpeed;
     public Rigidbody2D planeBody;
-    public Animator planeAnimation;
 
     // Start is called before the first frame update
     void Start()
