d7c93fe7aaeafc7bb99aa06aeb3a18a357552852
Josh Shinnick <asuraengine@hotmail.com>
2019-06-20 20:56:01 +0800

    20/06/2019
    Got Portal Spawning and Enemy spawning working as well as the next portal opening being pointed too.

---
+++ Assets/Scripts/Clouds/CloudDestroy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudDestroy : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameObject.transform.position.x < -15)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PointToPortal.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointToPortal : MonoBehaviour
+{
+    public SpawningEngine portalStat;
+    public Vector3 currentPortalPos;
+
+    public Transform playerPos;
+
+    public GameObject arrowPointer;
+
+    private void Update()
+    {
+        currentPortalPos = portalStat.curPortal.transform.position; //- Camera.main.WorldToScreenPoint(transform.position);
+        Debug.Log(currentPortalPos);
+        //currentPortalPos.z = 0;
+        //arrowPointer.transform.LookAt(currentPortalPos);
+        Vector2 pPos = playerPos.transform.position;
+        var angle = Mathf.Atan2(pPos.y-currentPortalPos.y, pPos.x-currentPortalPos.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle + 180f, Vector3.forward);
+    }
+}
