b31084c08ca3b40916a1db9dcebbcbca1a20b5bf
Josh Shinnick <asuraengine@hotmail.com>
2019-06-28 01:41:17 +0800

    28/06/2019
    Josh S. Version of a AI tracking system Will work for Week 6 presentation.

---
+++ Assets/Scripts/EnemyDogFighter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDogFighter : MonoBehaviour
+{
+    public Transform target;
+    public Rigidbody2D enemyBody;
+    public float angleChangingSpeed;
+    public float movementSpeed;
+
+    private void Awake()
+    {
+        target = GameObject.Find("Player").transform;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 direction = (Vector2)target.position - enemyBody.position;
+        direction.Normalize();
+        float rotateAmount = Vector3.Cross(direction, transform.right).z;
+        enemyBody.angularVelocity = -angleChangingSpeed * rotateAmount;
+        enemyBody.velocity = transform.right * movementSpeed;
+    }
+}
