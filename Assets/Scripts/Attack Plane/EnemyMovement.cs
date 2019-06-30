baa0aa6fd5ee795fce64f2267a0589d9972e0d4e
Josh <josh_mccormick@msn.com>
2019-06-30 18:15:58 +0800

    Added enemy homing missile & small tweaks
    homing missile prefab is missing the target transform (supposed to be player)
    homing missile thrust and turning angle will most likely need to be tweaked.

--- Assets/Scripts/Attack Plane/EnemyMovement.cs
+++ Assets/Scripts/Attack Plane/EnemyMovement.cs
@@ -7,14 +7,21 @@ public class EnemyMovement : MonoBehaviour
     public GameObject EnemyShot;
     public Transform EnemyBulletSpawn;
     public float fireRate;
-
     private float nextFire;
+
+    public GameObject EnemyMissile;
+    public Transform EnemyMissileSpawn;
+    public float missileCooldownTime = 5;
+    private float missileNextFireTime = 10;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (Time.time == 10)
+        {
+            missileCooldownTime = 0;
+        }
     }
 
     // Update is called once per frame
@@ -25,10 +32,17 @@ public class EnemyMovement : MonoBehaviour
             nextFire = Time.time + fireRate;
             Instantiate(EnemyShot, EnemyBulletSpawn.position, EnemyBulletSpawn.rotation);
         }
+
+        if ( Time.time > missileNextFireTime)
+        {
+            Instantiate(EnemyMissile, EnemyMissileSpawn.position, EnemyMissileSpawn.rotation);
+            missileNextFireTime = Time.time + missileCooldownTime;
+        }
+
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        
+        Destroy(gameObject);
     }
 }
---
+++ Assets/Scripts/HomingMissile.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class HomingMissile : MonoBehaviour
+{
+
+    public Transform playerTarget;
+
+    private Rigidbody2D rb;
+
+    //[SerializeField]
+    //GameCamera explosionEffect;
+
+    [SerializeField]
+    float thrust = 1;
+
+    [SerializeField]
+    float rotateSpeed = 200f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //playerTarget = GameObject.FindGameObjectWithTag("Player").transform;
+
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+
+        Vector2 direction = (Vector2)playerTarget.position - rb.position;
+        direction.Normalize();
+        float rotateAmount = Vector3.Cross(direction, transform.forward).z;
+        rb.angularVelocity = -rotateAmount * rotateSpeed;
+        rb.velocity = transform.forward * thrust;
+
+        rb.AddRelativeForce(Vector3.forward * thrust);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Instantiate(explosionEffect, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
