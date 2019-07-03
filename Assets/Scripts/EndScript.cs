8e8eeedb0b7310c4c505be037749f2d3d91b5e10
Brody Reynolds <1015760@student.sae.edu.au>
2019-07-03 13:12:47 +0800

    End Screen UI
    Menu to game works. Game to end screen being done by Josh. End screen back to game and survey both work.

---
+++ Assets/Scripts/EndScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndScript : MonoBehaviour
+{
+    public void restartgame()
+    {
+        SceneManager.LoadScene("WorldGen");
+    }
+
+    public void exittosurvey()
+    {
+        Application.OpenURL("https://docs.google.com/document/d/1SyO7DjQJscdXMUsuRROa0wt-U3ad5mETM2DFsP9pTvk/edit?usp=sharing");
+    }
+}
