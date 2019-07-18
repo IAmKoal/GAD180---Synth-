0123b76235e1b61340cccb348bd4504c1e70e203
JoshShinnick <asuraengine@hotmail.com>
2019-07-18 14:31:08 +0800

    During Class - Moving Comp
    As Above

--- Assets/Scripts/Player/LazerRasor.cs
+++ Assets/Scripts/Player/LazerRasor.cs
@@ -17,34 +17,36 @@ public class LazerRasor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        RaycastHit2D hit = Physics2D.Raycast(laserPoint.position, transform.right, 20, layerMask);
-
-        if (hit)
-        {
-            laserRender.SetPosition(0, laserPoint.position);
-            laserRender.SetPosition(1, hit.point);
-        }
-        else
-        {
-            laserRender.SetPosition(0, laserPoint.position);
-            laserRender.SetPosition(1, laserPoint.position + laserPoint.right * 20);
-        }
-
         if (Input.GetKey(KeyCode.X))
         {
+            RaycastHit2D hit = Physics2D.Raycast(laserPoint.position, transform.right, 20, layerMask);
+
+            if (hit)
+            {
+                laserRender.SetPosition(0, laserPoint.position);
+                laserRender.SetPosition(1, hit.point);
+            }
+
+            else
+            {
+                laserRender.SetPosition(0, laserPoint.position);
+                laserRender.SetPosition(1, laserPoint.position + laserPoint.right * 20);
+            }
+
+            if (hit.collider != null)
+            {
+                if (hit.collider.gameObject.tag == "Enemy Bi Plane")
+                {
+                    hit.collider.gameObject.GetComponent<EnemyAttackPlaneStats>().Damage(35 * Time.deltaTime);
+                }
+            }
+
             laserRender.enabled = true;
         }
+
         else
         {
             laserRender.enabled = false;
-        }
-
-        if (hit.collider != null)
-        {
-            if (hit.collider.CompareTag("Enemy Bi Plane"))
-            {
-                hit.collider.gameObject.GetComponent<EnemyAttackPlaneStats>().Damage(25 / Time.deltaTime);
-            }
         }
     }
 }
