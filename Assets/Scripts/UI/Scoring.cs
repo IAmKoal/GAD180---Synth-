9066ce010eef9b70a1b8c68d425e7b08769da399
Josh Shinnick <1016401@student.sae.edu.au>
2019-07-18 18:35:45 +0800

    18/07/2019 - Score System
    Score system and Multiplier Working

---
+++ Assets/Scripts/UI/Scoring.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Scoring : MonoBehaviour
+{
+    // Change to how many digits you want
+    public int scoreLength = 9;
+
+    // this is the score
+    string newScore = "000000000";
+    int totalScore;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.GetComponent<TMP_Text>().text = newScore;
+    }
+
+    public void UpdateScore(int score)
+    {
+        totalScore += score;
+        // score as a string
+        string scoreString = totalScore.ToString();
+
+        // get number of 0s needed
+        int numZeros = scoreLength - scoreString.Length;
+
+        newScore = "";
+        for (int i = 0; i < numZeros; i++)
+        {
+            newScore += "0";
+        }
+        newScore += scoreString;
+    }
+}
