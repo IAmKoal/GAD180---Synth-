146c0e5ba418df26f32dc766423d7c790e36df1f
Josh <josh_mccormick@msn.com>
2019-07-25 11:38:17 +0800

    Added Bomber Enemy

---
+++ Assets/Scripts/Enemy/Attack Plane/EnemyMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public GameObject EnemyShot;
+    public Transform EnemyBulletSpawn;
+    public float fireRate;
+    private float nextFire;
+
+    public GameObject EnemyMissile;
+    public Transform EnemyMissileSpawn;
+    public float missileCooldownTime = 5;
+    private float missileNextFireTime = 10;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Time.time == 10)
+        {
+            missileCooldownTime = 0;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(EnemyShot, EnemyBulletSpawn.position, EnemyBulletSpawn.rotation);
+        }
+
+        if ( Time.time > missileNextFireTime)
+        {
+            Instantiate(EnemyMissile, EnemyMissileSpawn.position, EnemyMissileSpawn.rotation);
+            Debug.Log("Incoming Enemy Missile Detected!");
+            missileNextFireTime = Time.time + missileCooldownTime;
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Enemy/Bomber Plane/BomberStats.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BomberStats : MonoBehaviour
+{
+
+    public float bomberMaxHealth = 200;
+    public float bomberCurrentHealth;
+    public SceneStuff sceneStuff;
+    public Multiplier multiplier;
+    private IEnumerator coroutine;
+
+    public GameObject respawnPrefab;
+    public GameObject[] respawns;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bomberCurrentHealth = bomberMaxHealth;
+        sceneStuff = GameObject.Find("SceneManager").GetComponent<SceneStuff>();
+        multiplier = GameObject.Find("Multiplier").GetComponent<Multiplier>();
+
+        if (respawns == null)
+            respawns = GameObject.FindGameObjectsWithTag("Bomber Respawn");
+        foreach (GameObject respawn in respawns)
+        {
+            Instantiate(respawnPrefab, respawn.transform.position, respawn.transform.rotation);
+            Debug.Log("Enemy Bomber Incoming");
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (bomberCurrentHealth <= 0)
+        {
+            sceneStuff.enemiesKilled += 1;
+            multiplier.KillEvent(1000);
+            Destroy(gameObject);
+        }
+        if (bomberCurrentHealth <= 51 && bomberCurrentHealth > 0)
+        {
+            gameObject.GetComponentInChildren<ParticleSystem>().Play();
+        }
+
+    }
+
+    public void Damage(float damage)
+    {
+        coroutine = DamageIndication(0.1f);
+        StartCoroutine(coroutine);
+        bomberCurrentHealth -= damage;
+    }
+
+    private IEnumerator DamageIndication(float time)
+    {
+        gameObject.GetComponent<SpriteRenderer>().material.SetFloat("_FlashAmount", 1);
+        yield return new WaitForSeconds(time);
+        gameObject.GetComponent<SpriteRenderer>().material.SetFloat("_FlashAmount", 0);
+    }
+
+}
