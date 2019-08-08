54757fba3d953a3130b6549f36c7aef064013eaa
Josh <josh_mccormick@msn.com>
2019-08-08 18:19:03 +0800

    8/08/19
    Added Bomber Shooting

---
+++ Assets/Scripts/Enemy/Bomber Plane/BomberShooting.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BomberShooting : MonoBehaviour
+{
+    [SerializeField]
+    GameObject bomb;
+
+    [SerializeField]
+    GameObject bombSpawn;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<>
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
