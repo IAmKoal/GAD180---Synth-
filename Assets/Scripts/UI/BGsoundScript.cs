5917f68613da194e0a832bbd87b31c8d17fd7c44
Brody Reynolds <1015760@student.sae.edu.au>
2019-08-09 17:52:55 +0800

    UI Overhaul and Back Ground Audio Overhaul
    UI for the main menu has been greatly altered but still has some work to be done. Back Ground Audio has also been done but not the in game audio for players/enemies.

---
+++ Assets/Scripts/UI/BGsoundScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGsoundScript : MonoBehaviour
+{
+    //Play Global
+    private static BGsoundScript instance = null;
+    public static BGsoundScript Instance
+    {
+        get { return instance; }
+    }
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+    //Play Global End
+}
