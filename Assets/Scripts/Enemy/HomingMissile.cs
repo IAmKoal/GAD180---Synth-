991619717aa21eb06fe6eb943fa171c187204020
Josh <josh_mccormick@msn.com>
2019-08-09 11:27:22 +0800

    9/08/19
    Fixed Homing Missile Tracking & Bombs

--- Assets/Scripts/Enemy/HomingMissile.cs
+++ Assets/Scripts/Enemy/HomingMissile.cs
@@ -6,40 +6,76 @@ public class HomingMissile : MonoBehaviour
 {
 
     public Transform playerTarget;
-    public Animator animator;
-    private Rigidbody2D rb;
+    public Animator explosion;
+    public Rigidbody2D rb;
+    public PlayerStats playerhealth, player;
 
     //[SerializeField]
     //GameCamera explosionEffect;
 
     [SerializeField]
-    float thrust = 20f;
-
+    float thrust = 30f, rotateSpeed = 200f, missileDamage = 35f, timeAlive, movementSpeed;
     [SerializeField]
-    float rotateSpeed = 200f;
+    bool isAlive = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-
+        timeAlive = 10;
         playerTarget = GameObject.FindGameObjectWithTag("Player").transform;
+        player = gameObject.GetComponent<PlayerStats>();
+    }
+
+    private void Update()
+    {
+        if (timeAlive >= 0)
+        {
+            timeAlive -= Time.deltaTime;
+        }
+        else
+        {
+            isAlive = false;
+            Destroy(gameObject);
+        }
     }
 
     void FixedUpdate()
     {
-        Vector2 direction = (Vector2)playerTarget.position - rb.position;
+        /*Vector2 direction = (Vector2)playerTarget.position - rb.position;
         direction.Normalize();
         float rotateAmount = Vector3.Cross(direction, transform.right).z;
         rb.angularVelocity = -rotateAmount * rotateSpeed;
         rb.velocity = transform.right * thrust;
+        */
+
+        if(isAlive == true)
+        {
+            Vector2 direction = (Vector2)playerTarget.position - rb.position;
+            direction.Normalize();
+            float rotateAmount = Vector3.Cross(direction, transform.right).z;
+            rb.angularVelocity = -rotateSpeed * rotateAmount;
+            rb.velocity = transform.right * movementSpeed;
+        }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D missileCollider)
     {
-        //Instantiate(explosionEffect, transform.position, transform.rotation);
-        rb.velocity = new Vector2 (0,0);
-        animator.SetBool("Hit", true);
-        Destroy(gameObject);
+        if(missileCollider.gameObject.tag == "Player")
+        {
+            missileCollider.GetComponent<PlayerStats>();
+            playerhealth.playerCurrentHealth -= missileDamage;
+            Debug.Log(playerhealth.playerCurrentHealth + "player hit by missile");
+            rb.velocity = new Vector2(0, 0);
+            isAlive = false;
+            explosion.SetBool("Hit", true);
+            Destroy(gameObject);
+        }
+        else
+        {
+            rb.velocity = new Vector2(0, 0);
+            isAlive = false;
+            explosion.SetBool("Hit", true);
+            Destroy(gameObject);
+        }
     }
 }
